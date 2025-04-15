bd7a9991b789e99634697a6fd80212f6678a91db
abheetgaur <abheetgaur92@gmail.com>
2025-04-15 14:56:50 +0530

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("registration-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the form from submitting for demo purposes
+  alert("Bidder registration submitted successfully!");
+});
